7b329d896bb4064c96780861b66f0763e32ecfcc
Milan Dzodic <milan.dzodic@hotmail.com>
2024-11-12 14:58:01 +0100

    G delen helt klar

--- src/components/ClientsLoving.jsx
+++ src/components/ClientsLoving.jsx
@@ -1,31 +1,32 @@
-import OurClientsQuote from "../assets/Images/OurClientsQuote.svg"
-import OurClientsReview1 from "../assets/Images/OurClientsReview1.svg"
-import OurClientsReview2 from "../assets/Images/OurClientsReview2.svg"
-
+import { useEffect, useState } from "react"
+import TestimoItem from "./TestimoItem"
 
 const Clientsloving = () => {
+  const [Testimo, setTestimo] = useState([])
+
+  const getTestimo = async () => {
+    const res = await fetch('https://win24-assignment.azurewebsites.net/api/testimonials')
+    setTestimo(await res.json())
+  }
+
+  useEffect(() => {
+    getTestimo()
+  }, [])
+
   return (
-    <>
-      <section id="clients-are-loving">
-        <div className="container">
-          <h1 className="headline">
-            Clients are Loving Our App
-          </h1>
+    <section id="clients-are-loving">
+      <div className="container">
+        <h1 className="headline">
+          Clients are Loving Our App
+        </h1>
 
-          <div className="images-1">
-            <img id="outer-first" src={OurClientsQuote} alt=""/>
-
-            <img id="inner-first" src={OurClientsReview1} alt=""/>
-          </div>
-
-          <div className="images-2">
-            <img id="outer-second" src={OurClientsQuote} alt=""/>
-            
-            <img id="inner-second" src={OurClientsReview2} alt="" />
-          </div>
+        <div className="tja">
+        {
+          Testimo.map((item) => (<TestimoItem key={item.id} item={item} />))
+        }
         </div>
-      </section>
-    </>
+      </div>
+    </section>
   )
 }
 export default Clientsloving
---
+++ src/components/TestimoItem.jsx
@@ -0,0 +1,35 @@
+import Review1 from "../assets/Images/Rating1.svg"
+import Review2 from "../assets/Images/Rating2.svg"
+
+const TestimoItem = ({ item }) => {
+
+  const starrating = item.starRating === 4 ? Review1 : Review2
+
+  return (
+    <div className="testimonials">
+      <div className="comment">
+        <p>{item.comment}</p>
+      </div>
+
+      <div className="tjabba">
+        <div className="starrating">
+          <img src={starrating}/>
+        </div>
+
+        <div className="avatarurl">
+          <img src={item.avatarUrl}/>
+        </div>
+        
+        <div className="author">
+          <h3>{item.author}</h3>
+        </div>
+        
+        <div className="jobrole">
+          <p>{item.jobRole}</p>
+        </div>
+      </div>
+
+    </div>
+  )
+}
+export default TestimoItem
